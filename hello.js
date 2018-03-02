f366237958db1a9ad7043e45c4f9dd041b3d2e49
Fernando Henrique <fernando88to@gmail.com>
2018-03-02 09:24:18 -0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Servidor Rodando 1.1\n');
+  res.end('Servidor Rodando 1.2\n');
 }).listen(8500, 'localhost');
 console.log('Servidor rodando http://localhost:8500/');
