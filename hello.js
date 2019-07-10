0d71c2b58544872bc0ae8f9dcf183c0bdb9176b7
fernando88to <fernando88to@gmail.com>
2019-07-10 18:30:55 -0300

    .

--- hello.js
+++ hello.js
@@ -3,6 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
 
     res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.end(JSON.stringify({ selo: "123456AAAA123456", tfj:15.3, funcivil:15.85 , emolumento:80}));
     //res.json({selo: "123456AAA123456"});
 }).listen(8500, 'localhost');
